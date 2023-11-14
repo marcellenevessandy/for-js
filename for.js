5c86bcf60f0d583053fcdb8dd3f5dd4914f0ceac
Marcelle Neves Sandy <146496555+marcellenevessandy@users.noreply.github.com>
2023-11-14 19:51:08 -0300

    Adicionei o arquivo.

---
+++ for.js
@@ -0,0 +1,12 @@
+// // For tradicional (let = var)
+// for (let contador = 0; contador < 10; contador++) {
+//     console.log ("Apredendo JavaScript")
+// }
+
+const numeros = [0,1,2,3,4,5];
+// For simples
+for (numero of numeros){
+    console.log ("Apredendo JavaScript")
+}
+
+
